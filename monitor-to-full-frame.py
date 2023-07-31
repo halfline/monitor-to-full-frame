42afed39af0b13dbe110150b5116b6b7b7b86bbf
Ray Strode <rstrode@redhat.com>
2023-07-31 16:38:03 -0400

    wip! Do two video passes and add zoom animation
    This reworks things to process the transformation matrices
    ahead of the output rendering so we can see ahead of time
    if we should apply the matrix or not. it also adds a zoom animation.
    
    It still needs to notice when part of the monitor is off camera so
    the transform matrix doesn't abruptly zoom in and out as the aspect
    ratio changes.
    
    I guess we'll have to somehow extrapolate how much of the monitor
    is off screen based on prior good frames to figure out monitor size.

--- monitor-to-full-frame.py
+++ monitor-to-full-frame.py
@@ -7,16 +7,28 @@ import threading
 import traceback
 from collections import deque
 from sortedcontainers import SortedDict
+from itertools import takewhile
 import gi
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst, GLib, GObject
 import queue
 import sys
+from enum import Enum, auto
+
 import json
 
 input_file = sys.argv[1] if len(sys.argv) > 1 else 'input.mp4'
 output_file = sys.argv[2] if len(sys.argv) > 2 else 'output.mp4'
 audio_loop = GLib.MainLoop()
+
+class ZoomState(Enum):
+    ZOOMED_OUT = auto()
+    ZOOMING_IN = auto()
+    ZOOMED_IN = auto()
+    ZOOMING_OUT = auto()
+
+def ease_out_quad(time):
+    return 1.0 - (1.0 - time)**2
 
 def on_message(bus, message):
     if message.type == Gst.MessageType.EOS:
@@ -93,7 +105,16 @@ def draw_biggest_object(image):
     cv2.drawContours(image, [hull], -1, (0, 255, 0), 3)
     return image
 
-def transform_image_to_be_axis_aligned(image, width, height, transformation_matrices, max_matrices):
+def draw_outline_on_where_to_zoom(image, matrix, width, height):
+    corners = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype='float32')
+    corners = corners.reshape(-1, 1, 2)
+    warped_corners = cv2.perspectiveTransform(corners, np.linalg.inv(matrix))
+    warped_corners = warped_corners.astype(int)
+    cv2.drawContours(image, [warped_corners], -1, (0, 255, 255), 3)
+
+    return image
+
+def calculate_transform_matrix(image, width, height):
     grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, quantized_image = cv2.threshold(grayscale_image, 100, 255, cv2.THRESH_BINARY)
     objects, _ = cv2.findContours(quantized_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -107,26 +128,35 @@ def transform_image_to_be_axis_aligned(image, width, height, transformation_matr
         unaligned_box_around_object = cv2.minAreaRect(hull)
         hull = np.intp(cv2.boxPoints(unaligned_box_around_object))
 
-    if len(hull) == 4:
-        input_points = order_points(hull.reshape(4, 2))
-        output_points = order_points(np.array([[0, 0], [width - 1, 0], [width - 1, height - 1], [0, height - 1]]))
+    if len(hull) != 4:
+        return None
 
-        input_rectangle = np.array(input_points, dtype=np.float32)
-        output_rectangle = np.array(output_points, dtype=np.float32)
-        matrix = cv2.getPerspectiveTransform(input_rectangle, output_rectangle)
+    input_points = order_points(hull.reshape(4, 2))
+    output_points = order_points(np.array([[0, 0], [width - 1, 0], [width - 1, height - 1], [0, height - 1]]))
 
-        transformation_matrices.append(matrix)
-        if len(transformation_matrices) > max_matrices:
-            transformation_matrices.pop(0)
+    input_rectangle = np.array(input_points, dtype=np.float32)
+    output_rectangle = np.array(output_points, dtype=np.float32)
+    matrix = cv2.getPerspectiveTransform(input_rectangle, output_rectangle)
 
-    if len (transformation_matrices) == 0:
-        return image
+    return matrix
 
-    average_matrix = np.mean(transformation_matrices, axis=0)
+def find_monitor_in_frames(input_file, transformation_matrices, condition, min_queue_length):
+    video = cv2.VideoCapture(input_file)
+    frame_number = 0
 
-    corrected_image = cv2.warpPerspective(image, average_matrix, (width, height))
+    while True:
+        success, image = video.read()
+        if not success:
+            break
+        transform_matrix = calculate_transform_matrix(image, width, height)
 
-    return corrected_image
+        transformation_matrices[frame_number] = transform_matrix
+        with condition:
+            if len(transformation_matrices) >= min_queue_length:
+                condition.notify()
+        frame_number += 1
+
+    video.release()
 
 def rotate_image (image, rotation):
     if rotation == 180:
@@ -137,53 +167,6 @@ def rotate_image (image, rotation):
         image = cv2.rotate (image, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
     return image
-
-def stabilize_image(image, transformations_buffer, max_transformations, state):
-    orb = cv2.ORB_create()
-
-    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    if state is None:
-        key_points, descriptors = orb.detectAndCompute(grayscale_image, None)
-        state = {'key-points': key_points, 'descriptors': descriptors}
-        return image, state
-
-    key_points, descriptors = orb.detectAndCompute(grayscale_image, None)
-
-    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
-    matches = matcher.match(state['descriptors'], descriptors)
-
-    if len(matches) < 200:
-        state['key-points'] = key_points
-        state['descriptors'] = descriptors
-        transformations_buffer.clear()
-        return image, state
-
-    matches = sorted(matches, key=lambda x: x.distance)
-
-    points1 = np.zeros((len(matches), 2), dtype=np.float32)
-    points2 = np.zeros((len(matches), 2), dtype=np.float32)
-
-    for i, match in enumerate(matches):
-        points1[i, :] = state['key-points'][match.queryIdx].pt
-        points2[i, :] = key_points[match.trainIdx].pt
-
-    h, _ = cv2.findHomography(points1, points2, cv2.RANSAC)
-
-    transformations_buffer.append(h)
-
-    if len(transformations_buffer) > max_transformations:
-        transformations_buffer.pop(0)
-
-    average_transform = np.mean(transformations_buffer, axis=0)
-
-    stabilized_image = cv2.warpPerspective(image, average_transform, (image.shape[1], image.shape[0]))
-
-    state['key-points'] = key_points
-    state['descriptors'] = descriptors
-
-    return stabilized_image, state
 
 def draw_audio_bars(image, width, height, audio_level, minimum_audio_level, maximum_audio_level):
     audio_bars_width = int(0.10 * width)
@@ -213,7 +196,7 @@ def draw_audio_bars(image, width, height, audio_level, minimum_audio_level, maxi
 
     return image
 
-def get_audio_levels_for_timestamp(audio_levels, timestamp, state):
+def get_audio_levels_for_timestamp(audio_levels, timestamp, state, condition):
     with condition:
         while not audio_levels or timestamp > audio_levels.peekitem(-1)[0]:
             condition.wait()
@@ -267,20 +250,31 @@ if success:
     fps = int(fps)
 
 audio_levels = SortedDict()
-condition = threading.Condition()
+audio_levels_changed_condition = threading.Condition()
 
 if success:
     print("Starting audio extraction")
-    thread = threading.Thread(target=extract_audio, args=(audio_levels, condition))
-    thread.start()
+    audio_thread = threading.Thread(target=extract_audio, args=(audio_levels, audio_levels_changed_condition))
+    audio_thread.start()
+
+warp_time = 2.5
+steady_time = 2.5
+min_transformation_matrices_length = int(fps * (warp_time * 2 + steady_time))
+transforms_condition = threading.Condition()
+transformation_matrices = {}
+if success:
+    print("Analyzing frames for monitor")
+    analyzer_thread = threading.Thread(target=find_monitor_in_frames, args=(f"{input_file}", transformation_matrices, transforms_condition, min_transformation_matrices_length))
+    analyzer_thread.start()
 
 i = 0
 rotation_matrices = []
 cropping_rects = []
-transformation_matrices = []
 transformations_buffer = deque(maxlen=fps)
-stabilization_state = None
 audio_state = None
+zoom_state = ZoomState.ZOOMED_OUT
+last_zoomed_out_frame = 0
+untransformed_matrix = np.eye(3, dtype=np.float32)
 
 try:
     rotation = get_rotation(input_file);
@@ -297,17 +291,56 @@ try:
 
         try:
             print("Drawing frame " + str(i));
+
             image = pristine_image
-            image = rotate_image (image, rotation)
             #image = draw_biggest_object (image)
-            number_of_frames_to_smooth=fps
-            image, stabilization_state = stabilize_image (image, transformations_buffer, number_of_frames_to_smooth, stabilization_state)
-            image = transform_image_to_be_axis_aligned(image, width, height, transformation_matrices, number_of_frames_to_smooth)
+            with transforms_condition:
+                while len(transformation_matrices) <= min_transformation_matrices_length:
+                    print(f"\rWaiting for matrices [{len(transformation_matrices)}/{min_transformation_matrices_length}]")
+                    transforms_condition.wait()
+
+            #if transformation_matrices[i] is not None:
+            #    image = draw_outline_on_where_to_zoom (image, transformation_matrices[i], width, height)
+
+            if zoom_state == ZoomState.ZOOMED_OUT:
+                consecutive_matrices = list(takewhile(lambda matrix: matrix is not None, transformation_matrices))
+                if len(consecutive_matrices) >= min_transformation_matrices_length:
+                    last_zoomed_out_frame = i
+                    zoom_state = ZoomState.ZOOMING_IN
+                    print("Zooming in")
+            elif zoom_state == ZoomState.ZOOMING_IN:
+                fraction = (i - last_zoomed_out_frame) / (warp_time * fps)
+                fraction = ease_out_quad (fraction)
+                print (f"zooming in {fraction * 100}%")
+                matrix = untransformed_matrix * (1 - fraction) + transformation_matrices[i] * fraction
+                image = cv2.warpPerspective (image, matrix, (width, height))
+                if fraction >= 1.0:
+                    zoom_state = ZoomState.ZOOMED_IN
+                    print("Done zooming in")
+            elif zoom_state == ZoomState.ZOOMED_IN:
+                image = cv2.warpPerspective (image, transformation_matrices[i], (width, height))
+
+                consecutive_matrices = list(takewhile(lambda matrix: matrix is not None, transformation_matrices))
+                if len(consecutive_matrices) < fps * (steady_time + warp_time):
+                    last_zoomed_in_frame = i
+                    zoom_state = ZoomState.ZOOMING_OUT
+                    print("Zooming out")
+            elif zoom_state == ZoomState.ZOOMING_OUT:
+                fraction = (i - last_zoomed_in_frame) / (warp_time * fps)
+                matrix = transformation_matrices[i] * (1 - fraction) + untransformed_matrix * fraction
+                image = cv2.warpPerspective (image, matrix, (width, height))
+                if fraction >= 1.0:
+                    zoom_state = ZoomState.ZOOMED_OUT
+                    print("Done zooming out")
+
+            del transformation_matrices[i]
+
+            image = rotate_image (image, rotation)
 
             timestamp = video.get(cv2.CAP_PROP_POS_MSEC) * 1e6
 
             if audio_loop.is_running():
-                audio_level, minimum_audio_level, maximum_audio_level, audio_state = get_audio_levels_for_timestamp (audio_levels, timestamp, audio_state)
+                audio_level, minimum_audio_level, maximum_audio_level, audio_state = get_audio_levels_for_timestamp (audio_levels, timestamp, audio_state, audio_levels_changed_condition)
                 image = draw_audio_bars(image, width, height, audio_level, minimum_audio_level, maximum_audio_level);
 
         except Exception as e:
@@ -324,6 +357,10 @@ except:
 if output:
     output.release()
 
-if audio_loop.is_running():
-    audio_loop.quit()
-thread.join()
+if audio_thread:
+    if audio_loop.is_running():
+        audio_loop.quit()
+    audio_thread.join()
+
+if analyzer_thread:
+    analyzer_thread.join()
